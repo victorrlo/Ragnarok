5d6a4063507f42b4cc631da5ca8a763a7b6aef05
victorrlo <42052096+victorrlo@users.noreply.github.com>
2025-02-21 22:31:15 -0300

    Modify character movement and add cursor package
    - Updated character movement to use `context.started` instead of `context.performed`
    - Adjusted character walking coroutine indentation
    - Added Unity cursor package from custom GitHub repository
    - Updated sprite import settings for various assets

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -10,9 +10,11 @@ public class CharacterMovement : MonoBehaviour
     float _moveAccuracy = 0.01f;
     Coroutine _walkCoroutine;
 
+
+    
     public void OnMouseClick(InputAction.CallbackContext context)
     {
-        if (context.performed)
+        if (context.started)
         {
             if(_walkCoroutine != null)
             {
@@ -21,8 +23,8 @@ public class CharacterMovement : MonoBehaviour
             _walkCoroutine = StartCoroutine(Walk());
         }
 
-        // actions usam contexto então a minha função tava sendo disparada 3x...
-        //como eu quero apenas 1 click para ativar o movimento, só preciso do performed
+        // actions usam contexto entï¿½o a minha funï¿½ï¿½o tava sendo disparada 3x...
+        //como eu quero apenas 1 click para ativar o movimento, sï¿½ preciso do performed
         // https://discussions.unity.com/t/player-input-component-triggering-events-multiple-times/781922/7
         //if (context.started) Debug.LogWarning("onMouseClickStarted");
         //else if (context.performed) Debug.LogWarning("onMouseClickPerformed");
@@ -31,26 +33,26 @@ public class CharacterMovement : MonoBehaviour
 
     public IEnumerator Walk()
     {
-        // para pegar posição do mouse, o melhor a usar é Camera.main.ScreenToWorlPoint(Input.mousePosition).x ou y
+        // para pegar posiï¿½ï¿½o do mouse, o melhor a usar ï¿½ Camera.main.ScreenToWorlPoint(Input.mousePosition).x ou y
         // https://stackoverflow.com/questions/33900150/object-doesnt-move-with-mouse-pointer
 
 
         // para mover o personagem a uma velocidade constante
         // https://www.youtube.com/watch?v=EhALudpeNRQ&list=PLzskWQnp3wmYJb-a0-b-P1v0R_SFGCisn&index=7
-        var targetPos = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+            var targetPos = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
 
-        Vector2 posDifference = targetPos - (Vector2)_player.position;
+            Vector2 posDifference = targetPos - (Vector2)_player.position;
 
-        while (posDifference.magnitude > _moveAccuracy)
-        {
-            _player.Translate(_movementSpeed * posDifference.normalized * Time.deltaTime);
-            posDifference = targetPos - (Vector2)_player.position;
+            while (posDifference.magnitude > _moveAccuracy)
+            {
+                _player.Translate(_movementSpeed * posDifference.normalized * Time.deltaTime);
+                posDifference = targetPos - (Vector2)_player.position;
+                yield return null;
+            }
+
+            _player.position = targetPos;
+
+            _walkCoroutine = null;
             yield return null;
-        }
-
-        _player.position = targetPos;
-
-        _walkCoroutine = null;
-        yield return null;
     }
 }
